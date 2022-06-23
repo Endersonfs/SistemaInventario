780485e734ddce4e613c8ce082552e1824d42b6f
Enderson Florian <eflorian@phoenixcalibrationdr.com>
2022-06-23 12:09:42 -0400

    Instalar Razor Runtime Compilation

--- SistemaInventario/Program.cs
+++ SistemaInventario/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
